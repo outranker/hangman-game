dbf2cd19e4aeecef7fcb7e4830a248122fa07d6d
Justin <outranker222@gmail.com>
2021-04-26 00:17:43 +0900

    added oxford api

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,28 +1,71 @@
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-    if (response.status === 200) {
-        const data = await response.json()
-        return data.puzzle
-    } else {
-        throw new Error('Unable to get puzzle')
-    }
-}
+  getDefinition();
+  const response = await fetch(
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error("Unable to get puzzle");
+  }
+};
 
 const getCountry = async (countryCode) => {
-    const response = await fetch(`//restcountries.eu/rest/v2/all`)
-    if (response.status === 200) {
-        const data = await response.json()
-        return data.find((country) => country.alpha2Code === countryCode)
-    } else {
-        throw new Error('Unable to fetch data')
-    }
-}
+  const response = await fetch(`//restcountries.eu/rest/v2/all`);
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find((country) => country.alpha2Code === countryCode);
+  } else {
+    throw new Error("Unable to fetch data");
+  }
+};
 
 const getLocation = async () => {
-    const response = await fetch('//ipinfo.io/json?token=078434010929a4')
-    if (response.status === 200) {
-        return response.json()
-    } else {
-        throw new Error('Unable to fetch the data')
+  const response = await fetch("//ipinfo.io/json?token=078434010929a4");
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error("Unable to fetch the data");
+  }
+};
+
+const getDefinition = async (word, type) => {
+  const app_id = "id"; //"my_app_id"; // insert your APP Id
+  const app_key = "key"; //"my_app_key"; // insert your APP Key
+  const wordId = "ace";
+  const fields = "pronunciations";
+  const strictMatch = "false";
+  const options = {
+    host: "od-api.oxforddictionaries.com",
+    port: "443",
+    path:
+      "/api/v2/entries/en-gb/" +
+      wordId +
+      "?fields=" +
+      fields +
+      "&strictMatch=" +
+      strictMatch,
+    method: "GET",
+    headers: {
+      app_id: app_id,
+      app_key: app_key,
+    },
+  };
+  //   if (type === "adjective") {
+  const response = await fetch(
+    "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace?strictMatch=false",
+    {
+      method: "GET",
+      mode: "cors",
+      Accept: "application/json",
+      headers: {
+        app_id: "6d5ed903",
+        app_key: "dc8d99a472c24587f0be8d1bff6e993b",
+      },
     }
-}
+  );
+  console.log("this is response definition", response.json());
+  //   } else {
+  //   }
+};
